05e23f185c1e175bdc0f440a7ec91027ede58b41
Paramananthan T. <UT01373@unicomtic.com>
2024-11-22 18:38:55 +0530

    user routing

--- src/app/components/user-profile/user-profile.component.ts
+++ src/app/components/user-profile/user-profile.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html',
   styleUrl: './user-profile.component.css'
 })
-export class UserProfileComponent {
+export class UserProfileComponent implements OnInit {
+  userInfo! : FormGroup
+  constructor(private fb : FormBuilder , private userService : UserService){
+    this.userInfo = this.fb.group({
+      nicNumber : ['' , [Validators.required]],
+      firstName : [''],
+      lastName : [''],
+      email: ['' , [Validators.required , Validators.email]],
+      contactNo : [''],
+      address : [''],
+      role: ['' , [Validators.required]],
+    })
+  }
+
+  ngOnInit(): void {
+    let user = JSON.parse(localStorage.getItem('user') || '');
+    this.userService.getUserById(user.NICNo).subscribe(data => {
+      console.log(data);
+      if(data){
+        this.userInfo.patchValue(data);
+      }
+    })
+  }
+  onEdit(){
+    console.log(this.userInfo.value)
+  }
 
 }
