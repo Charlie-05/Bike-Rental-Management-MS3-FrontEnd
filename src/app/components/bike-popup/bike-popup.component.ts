aedc9587abfd6742c3f0d9d2bfd39435b91dd75c
Paramananthan T. <UT01373@unicomtic.com>
2024-11-23 15:30:40 +0530

    page load bug

--- src/app/components/bike-popup/bike-popup.component.ts
+++ src/app/components/bike-popup/bike-popup.component.ts
@@ -10,25 +10,30 @@ import { ToastrService } from 'ngx-toastr';
   styleUrl: './bike-popup.component.css'
 })
 export class BikePopupComponent {
-  @Input() bikeData : any = '' ;
-  rentalRequestForm : FormGroup;
-  user = JSON.parse(localStorage.getItem('user') || '');
-  
-  constructor(private fb : FormBuilder , private rentalRequestService : RentalRequestService, private toastr : ToastrService){
-    this.rentalRequestForm = this.fb.group({
-      requestTime : [''],
-      bikeId : [''],
-      userId : [this.user.NICNo]
+  @Input() bikeData: any = '';
+  rentalRequestForm: FormGroup;
+
+
+  constructor(private fb: FormBuilder, private rentalRequestService: RentalRequestService, private toastr: ToastrService) {
+    let getUser = (localStorage.getItem('user'))
+    let user = {NICNo : ''}
+    if (getUser) {
+      user = JSON.parse(getUser);
+    } this.rentalRequestForm = this.fb.group({
+      requestTime: [''],
+      bikeId: [''],
+      userId: [user.NICNo]
     })
+
   }
 
-  onRequest(){
+  onRequest() {
     this.rentalRequestForm.value.bikeId = this.bikeData.id;
     console.log(this.rentalRequestForm.value);
     this.rentalRequestService.postRequest(this.rentalRequestForm.value).subscribe(data => {
       console.log(data);
-    },error=>{
-       this.toastr.error(error.error) ;
+    }, error => {
+      this.toastr.error(error.error);
     })
   }
 }
